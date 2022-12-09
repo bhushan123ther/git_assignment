a17776fd4635ec7e4e508ca65b80fb00e7d603e9
Bhushan Ther <bhushanther123@gmail.com>
2022-12-09 20:46:54 +0530

    prime code added

---
+++ prime.js
@@ -0,0 +1,19 @@
+function isPrime(num)
+{
+    let count = 0;
+    for(let i=1; i<=num.length; i++)
+    {
+        if(num%i==0)
+        {
+            count++;
+        }
+    }
+    if(count = 2)
+    {
+        console.log("Prime number");
+    }
+    else
+    {
+        console.log("Not Prime number");
+    }
+}
