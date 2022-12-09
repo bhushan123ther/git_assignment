0f466d4067bf22f974be32f048bebd48652d8c93
Bhushan Ther <bhushanther123@gmail.com>
2022-12-09 22:50:16 +0530

    code updated successfully

--- prime.js
+++ prime.js
@@ -1,14 +1,14 @@
 function isPrime(num)
 {
-    let count = 0;
+    let factor = 0;
     for(let i=1; i<=num.length; i++)
     {
         if(num%i==0)
         {
-            count++;
+            factor++;
         }
     }
-    if(count = 2)
+    if(factor == 2)
     {
         console.log("Prime number");
     }
